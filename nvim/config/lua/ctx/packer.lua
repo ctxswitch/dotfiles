4ec3e8c998a24c1ed349df92dddfbd7fadc93a0c
Rob Lyon <rob@ctxswitch.com>
2023-08-29 10:04:13 -0700

    remove nvim-tree

--- nvim/config/lua/ctx/packer.lua
+++ nvim/config/lua/ctx/packer.lua
@@ -2,8 +2,6 @@ vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
 	use('wbthomason/packer.nvim')
-	use('nvim-tree/nvim-tree.lua')
-	use('nvim-tree/nvim-web-devicons')
 	use('nvim-lualine/lualine.nvim')
 	use('mfussenegger/nvim-dap')
 	use('mfussenegger/nvim-lint')
